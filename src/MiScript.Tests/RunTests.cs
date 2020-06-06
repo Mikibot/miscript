05c7e62f09f8b716b3b0c3dd2815dc9bbc8ddfaa
Gerard Smit <gsmit1996@gmail.com>
2020-06-07 01:48:30 +0200

    Added unit tests for ScriptPacker

--- src/MiScript.Tests/RunTests.cs
+++ src/MiScript.Tests/RunTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MiScript.Models;
 using Xunit;
 using Xunit.Abstractions;
 using YamlDotNet.RepresentationModel;
@@ -24,28 +25,42 @@ namespace MiScript.Tests
         [MemberData(nameof(GetData))]
         public void Run(ScriptTestData test)
         {
-            var tokens = Tokenizer.Parse(test.Script);
-            var parser = new Parser.Parser(tokens.Tokens);
+            RunTest(test, false);
+        }
+        
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void RunPacked(ScriptTestData test)
+        {
+            RunTest(test, true);
+        }
+
+        private void RunTest(ScriptTestData test, bool pack)
+        {
+            var result = Tokenizer.Parse(test.Script);
 
             _output.WriteLine(test.Path);
             _output.WriteLine(new string('-', test.Path.Length));
 
-            var tokenLength = tokens.Tokens.Max(t => t.TokenType.ToString().Length) + 2;
+            var tokenLength = result.Tokens.Max(t => t.TokenType.ToString().Length) + 2;
 
             var sb = new StringBuilder();
 
-            if (tokens.Warnings.Count > 0)
+            if (result.Warnings.Count > 0)
             {
                 _output.WriteLine("Warnings:");
-                foreach (var warning in tokens.Warnings)
+                foreach (var warning in result.Warnings)
                 {
                     _output.WriteLine("[" + warning.Range.StartLine + ":" + warning.Range.StartColumn + "] " + warning.Message);
                 }
+
                 _output.WriteLine("");
             }
 
+            var tokens = result.Tokens;
+
             _output.WriteLine("Tokens:");
-            foreach (var token in tokens.Tokens)
+            foreach (var token in tokens)
             {
                 var type = token.TokenType.ToString();
                 sb.Append(type);
@@ -57,9 +72,20 @@ namespace MiScript.Tests
                     sb.Append(token.Value);
                     sb.Append('"');
                 }
-                
+
                 _output.WriteLine(sb.ToString());
                 sb.Clear();
+            }
+
+            if (pack)
+            {
+                var package = ScriptPacker.Pack(tokens);
+
+                _output.WriteLine("");
+                _output.WriteLine("Packaged:");
+                _output.WriteLine(ScriptPacker.ToString(package, true));
+
+                tokens = ScriptPacker.Unpack(package);
             }
 
             Dictionary<string, object> context;
@@ -74,15 +100,16 @@ namespace MiScript.Tests
 
                 context = deserializer.Deserialize<Dictionary<string, object>>(test.Model);
             }
-            
-            var result = parser.Parse(context);
+
+            var parser = new Parser.Parser(tokens);
+            var actual = parser.Parse(context);
 
             if (test.Expected != null)
             {
-                Assert.Equal(test.Expected, result);
+                Assert.Equal(test.Expected, actual);
             }
         }
-        
+
         public static IEnumerable<object[]> GetData()
         {
             var assembly = typeof(RunTests).Assembly;
